ad8304892d1fb941cce593344c9eb28b2f6f7bef
Thomas Flemmer <thomas.flemmer@mni.thm.de>
2013-08-16 16:46:42 +0200

    add usersearch

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,5 @@
 class UserController < ApplicationController
+	#has_many :friends
+	has_many :friends, Classname:friends, throught: :friendships 
+	has_many :buddies, Classname:friends, throught: :friends 
 end
