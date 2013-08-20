693623a7eb30098264d146624075d535babd6757
eert69 <erich.ertmann@mni.thm.de>
2013-08-20 16:01:36 +0200

    Restliche Elemente von Avatar for Rails entfernt

--- db/migrate/20130820112737_create_avatars_for_rails_user.rb
+++
@@ -1,9 +0,0 @@
-class CreateAvatarsForRailsUser < ActiveRecord::Migration
-  def up    
-    add_attachment :users, :logo
-  end
-  
-  def self.down
-    remove_attachment :users, :logo
-  end
-end
