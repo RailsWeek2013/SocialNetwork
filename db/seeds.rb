2e951152ba34e2134f2b52ddbc872ba53d53a969
Thomas Flemmer <thomas.flemmer@mni.thm.de>
2013-08-23 11:57:40 +0200

    remove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-u1 = User.create(firstname: "hans", lastname: "Peter", email: "hans@peter.de", password: "12345678", password_confirmation: "12345678")
-u2 = User.create(firstname: "susi", lastname: "Peter", email: "susi@peter.de", password: "12345678", password_confirmation: "12345678")
-u3 = User.create(firstname: "michi", lastname: "Peter", email: "michi@peter.de", password: "12345678", password_confirmation: "12345678")
+#u1 = User.create(firstname: "hans", lastname: "Peter", email: "hans@peter.de", password: "12345678", password_confirmation: "12345678")
+#u2 = User.create(firstname: "susi", lastname: "Peter", email: "susi@peter.de", password: "12345678", password_confirmation: "12345678")
+#u3 = User.create(firstname: "michi", lastname: "Peter", email: "michi@peter.de", password: "12345678", password_confirmation: "12345678")
 
 #u1.friends << u2
 #u1.friends << u3
